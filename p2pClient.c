7bb9978e16528586c2c3acb0d5808031381a8655
jonathanschlitt <development@jonathanschlitt.de>
2022-02-02 13:59:00 +0100

    added comments to all new methods + fixed prints

--- p2pClient.c
+++ p2pClient.c
@@ -176,7 +176,7 @@ void* serverFunction(void* arg) {
     // read the message from client and copy it in buffer
     read(sockfd, buffer, sizeof(buffer));
     // exchange upper-case letters by lower-case letter and vice versa.
-    printf("Server got message %s\n", buffer);
+    printf("Server: got message: %s\n", buffer);
     changeCase(buffer);
 
     // and send that buffer to client
@@ -327,52 +327,72 @@ int firstTouchClient(char* _serverAddress, int _port) {
 
 // +++++++++++++++++++ ClientServer Logic +++++++++++++++++++
 
-// preparing message for sending to client
+// Preparing global message for sending to server using one of five clients
 int setMessageForClient(int _id, char* _str) {
   if (_id >= MAX_CLIENTS) {
     fprintf(stderr, "Error: no such client\n");
     return -1;
   }
+  // Checking if is set to -1 which means it is available
   pthread_mutex_lock(&messageMut);
   if (Message.clientID != -1) {
     pthread_mutex_unlock(&messageMut);
     return 0;
   }
+  // Setting clientID of global message to id of target
   Message.clientID = _id;
   strncpy(Message.mesg, _str, MAX_MSG_SIZE);
   pthread_mutex_unlock(&messageMut);
+  // returning 1 for check if the logic in here worked
   return 1;
 }
 
 // accessing the client thread and printing out the message
 void* ThreadFunc(void* _data) {
+  // Casting ThreadData Pointer to data
   struct ThreadData* data = (struct ThreadData*)_data;
+
+  // Lock threadData to use
   pthread_mutex_lock(&threadDataMut);
-  // id from client
+  // Id from client
   int myID = data->ID;
   struct MessageForClient* mfc = data->mfc;
+
+  // unlocking mutex
   pthread_mutex_unlock(&threadDataMut);
   char buffer[MAX_MSG_SIZE];
 
+  // handling read and write of client with given id
   while (1) {
     pthread_mutex_lock(&messageMut);
     if (mfc->clientID == myID) {
       strncpy(buffer, mfc->mesg, MAX_MSG_SIZE);
-      printf("Used client id %d\n", data->connfd);
+      // printf("Used client id %d\n", data->connfd);
+
+      // Checking for write error
       if (write(data->connfd, buffer, sizeof(buffer)) == -1) {
-        printf("write error \n ");
+        printf("Error: Writing to server socket failed. \n ");
       }
 
-      printf("%d: sent message: %s\n", myID, buffer);
+      // Printing information that message was sent
+      printf("%d: sent message: %s\n", myID + 1, buffer);
       char response[MAX_MSG_SIZE];
       bzero(response, sizeof(response));
+
+      // Checking for read error
       if (read(data->connfd, response, sizeof(response)) == -1) {
-        printf("error\n");
+        printf("Error: Reading response from server socket failed. \n");
       }
 
-      printf("%d: got message: %s\n", myID, response);
+      // Printing returned message from server to console
+      printf("%d: got message: %sfrom server \n", myID + 1, response);
+
+      // Reset message id after usage
       mfc->clientID = -1;
+
+      // Unlocking Mutex of global message
       pthread_mutex_unlock(&messageMut);
+
       if (strncmp(buffer, "quit", 4) == 0) break;
       bzero(buffer, sizeof(buffer));
     } else {
@@ -384,23 +404,30 @@ void* ThreadFunc(void* _data) {
 }
 
 void readAndSendMessage(int clientNumber) {
+  // Locking the mutex to use the global threadData
   pthread_mutex_lock(&threadDataMut);
   printf("ClientId: %d\n", clientNumber);
+
+  // Checking if there is an connection made with the client which has the given
+  // id If thre already is the default value -1, there is no connection ==>
+  // leaving method
   if (threadInfo[clientNumber].connfd == -1) {
     printf("Verbindung %d existiert nocht nicht bitte mit C erstellen\n",
            clientNumber + 1);
     pthread_mutex_unlock(&threadDataMut);
     return;
   }
+  // unlocking Muter after reading data
   pthread_mutex_unlock(&threadDataMut);
 
-  // reading message from destination and message from commandline
+  // Setting Information about target and clientID
   char buffer[MAX_MSG_SIZE];
   int target = clientNumber;
-  Message.clientID = -1;
-  // char buffer[MESG_SIZE];
-  int n;
 
+  // Setting clientID of global message to -1
+  Message.clientID = -1;
+
+  // Waiting for clientID of global message to be set to -1
   while (1) {
     pthread_mutex_lock(&messageMut);
     if (Message.clientID == -1) {
@@ -411,36 +438,33 @@ void readAndSendMessage(int clientNumber) {
     usleep(100);
   }
 
-  // clear buffer
+  // Reading entered message to send over the client
+
+  // Clear buffer
   bzero(buffer, sizeof(buffer));
+
+  int n = 0;
   printf("Enter string: ");
-  n = 0;
+
   while ((buffer[n++] = getchar()) != '\n')
     ;
 
-  // while (Message.clientID != -1) {
-  //   usleep(100);
-  // }
-  // // reading inputs
-  // printf("Ziel: ");
-  // scanf("%d", &target);
-  // printf("Nachricht: ");
-  // scanf("%s", buffer);
-
-  // Sleep when sending message to client
+  // Set the global Message for client in combination with target
   while (!setMessageForClient(target, buffer)) usleep(100);
   pthread_mutex_lock(&messageMut);
+
+  // Unlocking MessageMutex that the global message can bes used by other client
+  // threads
   pthread_mutex_unlock(&messageMut);
-
-  // write(sockfd, buffer, sizeof(buffer));
-  // bzero(buffer, sizeof(buffer));
-
-  // read(sockfd, buffer, sizeof(buffer));
-  // printf("From Server : %s", buffer);
 }
 
 void establishConnection() {
   pthread_mutex_unlock(&threadDataMut);
+
+  // setting connection id flag to -1
+  // if there is no -1 found in the threadInfo array (which means there is space
+  // for client) then connectionId stays -1 ==> the next if statement will be
+  // visited
   int connectionId = -1;
   for (int i = 0; i < MAX_CLIENTS; i++) {
     if (threadInfo[i].connfd == -1) {
@@ -448,6 +472,9 @@ void establishConnection() {
       break;
     }
   }
+
+  // Check if there are clients left to connect with
+  // then leaving method
   if (connectionId == -1) {
     printf("Bereits %d Verbindungen Aufgebaut, keine übrig\n", MAX_CLIENTS);
     pthread_mutex_unlock(&threadDataMut);
@@ -455,11 +482,13 @@ void establishConnection() {
   }
   threadInfo[connectionId].ID = connectionId;
 
+  // Handling connection information
   char serverAddress[MESG_SIZE];
   int n1;
 
-  // clear buffer
+  // Clear buffer
   bzero(serverAddress, sizeof(serverAddress));
+
   printf("Enter IP Address: ");
   n1 = 0;
   while ((serverAddress[n1++] = getchar()) != '\n')
@@ -468,22 +497,30 @@ void establishConnection() {
   char portString[MESG_SIZE];
   int n2;
 
-  // clear buffer
+  // Clear buffer
   bzero(portString, sizeof(portString));
+
+  // Reading all given Informations with buffer
   printf("Enter Port: ");
   n2 = 0;
   while ((portString[n2++] = getchar()) != '\n')
     ;
 
+  // Printing output to console
   printf("IP Address: %s", serverAddress);
   printf("Port: %s", portString);
 
+  // Converting connection port to Integer
   int connectionPort = atoi(portString);
+
+  // Getting permanent port of server using firstTouchClient
   int permanentPort = firstTouchClient(serverAddress, connectionPort);
   int retries = 5;
 
+  // Saving connfd and handling connection errors
   while (threadInfo[connectionId].connfd < 0 && retries > 0) {
     sleep(1);
+    // Writing connfd to threadInfo array
     threadInfo[connectionId].connfd =
         setupConnectionClient(serverAddress, permanentPort);
     retries--;
@@ -497,6 +534,7 @@ void establishConnection() {
   pthread_mutex_unlock(&threadDataMut);
 }
 
+// TODO
 void stopConnection() {
   char buffer[MESG_SIZE];
   int n;
@@ -509,10 +547,9 @@ void stopConnection() {
     ;
 
   printf("Stop connection: Client %s", buffer);
-
-  // socket
 }
 
+// TODO
 void quit() {
   printf("Stopping server... \n");
   // sleep(100);
@@ -520,9 +557,11 @@ void quit() {
 }
 
 void waitForInput() {
+  // Initializing buffer and count n for input
   char buffer[MESG_SIZE];
   int n;
 
+  // Now waiting for user to Input a command
   while (1) {
     // clear buffer
     bzero(buffer, sizeof(buffer));
@@ -530,31 +569,29 @@ void waitForInput() {
     n = 0;
     while ((buffer[n++] = getchar()) != '\n')
       ;
-    //(buffer[0] = tolower(getchar()) != '\n');
-    // tolower(getchar())
 
-    // write(sockfd, buffer, sizeof(buffer));
-    // bzero(buffer, sizeof(buffer));
-
-    // read(sockfd, buffer, sizeof(buffer));
-    // printf("From Server : %s", buffer);
-
+    // Executing method to establish a new server connection
     if ((strncmp(buffer, "C", 1)) == 0) {
       printf("Your input: C\n");
       establishConnection();
       break;
     }
+
+    // Stop connection to server of a specific client
     if ((strncmp(buffer, "D", 1)) == 0) {
       printf("Your input: D\n");
       stopConnection();
       break;
     }
+
+    // Shutting down the whole p2p client and clsoing all threads
     if ((strncmp(buffer, "Q", 1)) == 0) {
       printf("Your input: Q\n");
       quit();
       break;
     }
 
+    // checking numbers of clients
     int command = buffer[0] - '0';
     if (0 <= command && command <= MAX_CLIENTS) {
       printf("Your input: %d\n", command);
@@ -567,15 +604,19 @@ int main(int argc, char** argv) {
   signal(SIGPIPE, SIG_IGN);  // Stopping Server crash after ctrl + c
   setbuf(stdout, 0);         // ==> deleting standard out puffer
 
-  // starting thread for server
+  // Starting thread for server
   pthread_t server;
   pthread_create(&server, NULL, &runServer, NULL);
 
-  // pthread_join(server, NULL);
-
+  // threadDataMutex to lock and unlock threadData Struct
   pthread_mutex_init(&threadDataMut, NULL);
   pthread_mutex_lock(&threadDataMut);
+
+  // Initialising clientID of global message as -1
   Message.clientID = -1;
+
+  // Setting all threadInfos of the 5 clients to default value
+  // if conffd is -1 this means this client can be used for a new connection
   for (int i = 0; i < MAX_CLIENTS; i++) {
     threadInfo[i].ID = i;
     threadInfo[i].mfc = &Message;
@@ -584,13 +625,14 @@ int main(int argc, char** argv) {
   pthread_mutex_unlock(&threadDataMut);
   pthread_mutex_init(&messageMut, NULL);
 
-  // initialising Client Threads
+  // Initialising Client Threads
   for (int i = 0; i < 5; i++) {
     struct ThreadData* clientArgument = malloc(sizeof(*clientArgument));
     clientArgument = &threadInfo[i];
     pthread_create(&clients[i], NULL, ThreadFunc, clientArgument);
   }
 
+  // Loop to wait for user input
   while (1) {
     waitForInput();
   }
